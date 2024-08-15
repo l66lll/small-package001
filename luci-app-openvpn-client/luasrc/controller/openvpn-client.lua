ce0eefeab39f084d2b459eaeb9b64283373d22f5
l66lll <nnnnfffs111@sina.com>
2024-08-15 12:06:26 +0800

    Update openvpn-client.lua

--- luci-app-openvpn-client/luasrc/controller/openvpn-client.lua
+++ luci-app-openvpn-client/luasrc/controller/openvpn-client.lua
@@ -1,10 +1,10 @@
 -- Copyright 2021-2022 Lienol <lawlienol@gmail.com>
-module("luci.controller.openvpn-client", package.seeall)
+module("luci.controller.anti-client", package.seeall)
 
 function index()
-    if not nixio.fs.access("/etc/config/luci-app-openvpn-client") then return end
+    if not nixio.fs.access("/etc/config/luci-app-Anti-client") then return end
 
     entry({"admin", "services"}, firstchild(), "services", 10).dependent = false
-    entry({"admin", "services", "openvpn-client"}, cbi("openvpn-client/settings"), _("OpenVPN Client"), 50).acl_depends = { "luci-app-openvpn-client" }
-    entry({"admin", "services", "openvpn-client", "client"}, cbi("openvpn-client/client")).leaf = true
+    entry({"admin", "services", "anti-client"}, cbi("anti-client/settings"), _("Anti Client"), 50).acl_depends = { "luci-app-anti-client" }
+    entry({"admin", "services", "anti-client", "client"}, cbi("anti-client/client")).leaf = true
 end
